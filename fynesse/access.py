f8224402afd5edfb508b76ce90f7ce0126a6150a
Heidar-An <heidaranwari@gmail.com>
2024-11-21 16:01:30 +0000

    random functions

--- fynesse/access.py
+++ fynesse/access.py
@@ -30,6 +30,8 @@ import requests
 import csv
 import warnings
 import osmnx as ox
+import zipfile
+import io
 
 
 def download_price_paid_data(year_from, year_to):
@@ -182,3 +184,25 @@ def get_osm_buildings_near_coordinates(latitude, longitude, bbox_side=1.0):
         pois["has_address"] = pois["addr_full"].str.strip() != ", ,"
         pois["area_sqm"] = pois.geometry.area
         return pois
+
+
+
+def download_census_data(code, base_dir=''):
+  url = f'https://www.nomisweb.co.uk/output/census/2021/census2021-{code.lower()}.zip'
+  extract_dir = os.path.join(base_dir, os.path.splitext(os.path.basename(url))[0])
+
+  if os.path.exists(extract_dir) and os.listdir(extract_dir):
+    print(f"Files already exist at: {extract_dir}.")
+    return
+
+  os.makedirs(extract_dir, exist_ok=True)
+  response = requests.get(url)
+  response.raise_for_status()
+
+  with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
+    zip_ref.extractall(extract_dir)
+
+  print(f"Files extracted to: {extract_dir}")
+
+def load_census_data(code, level='msoa'):
+  return pd.read_csv(f'census2021-{code.lower()}/census2021-{code.lower()}-{level}.csv')
--- fynesse/assess.py
+++ fynesse/assess.py
@@ -13,24 +13,33 @@ import sklearn.feature_extraction"""
 Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
 import matplotlib.pyplot as plt
 import osmnx as ox
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import Ridge, Lasso
+from sklearn.model_selection import cross_val_score
+from statistics import mean
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from scipy.stats import pearsonr
+import statsmodels.api as sm
 
 
-def data():
-    """Load the data from access and ensure missing values are correctly encoded as well as indices correct, column names informative, date and times correctly formatted. Return a structured data structure such as a data frame."""
-    df = access.data()
-    raise NotImplementedError
+# def data():
+#     """Load the data from access and ensure missing values are correctly encoded as well as indices correct, column names informative, date and times correctly formatted. Return a structured data structure such as a data frame."""
+#     df = access.data()
+#     raise NotImplementedError
 
-def query(data):
-    """Request user input for some aspect of the data."""
-    raise NotImplementedError
+# def query(data):
+#     """Request user input for some aspect of the data."""
+#     raise NotImplementedError
 
-def view(data):
-    """Provide a view of the data that allows the user to verify some aspect of its quality."""
-    raise NotImplementedError
+# def view(data):
+#     """Provide a view of the data that allows the user to verify some aspect of its quality."""
+#     raise NotImplementedError
 
-def labelled(data):
-    """Provide a labelled set of data ready for supervised learning."""
-    raise NotImplementedError
+# def labelled(data):
+#     """Provide a labelled set of data ready for supervised learning."""
+#     raise NotImplementedError
 
 def plot_buildings(pois, latitude, longitude, bbox_side):
     north, south, east, west = access.get_bbox(latitude, longitude, bbox_side)
@@ -57,3 +66,94 @@ def plot_buildings(pois, latitude, longitude, bbox_side):
     plt.legend()
     plt.tight_layout()
     plt.show()
+
+
+cols = ["L1-L3", "L4-L6", "L7", "L8-L9", "L10-11", "L12", "L13", "L14.1-L14.2", "L15"]
+
+def plot_label_model(ages, population, results, label = "UK", model_type = "Linear"):
+    plt.figure(figsize=(10,6))
+    plt.scatter(ages, population, label = "population")
+
+    plt.plot(ages, results.predict(), 'r-', label = f"{label} {model_type} Model")
+
+
+    plt.legend()
+    plt.xlabel("Age")
+    plt.ylabel("Population")
+    plt.title("Age Distribution and Model Fits")
+    plt.show()
+
+def plot_for_features(norm_age_df, columns_to_drop, column_names):
+    access.download_census_data('TS062')
+    # print(norm_age_df.shape)
+    student_df = access.load_census_data('TS062', "ltla")
+    # keep geography + L15 (full time students) + total residents
+    student_df = student_df.drop(student_df.columns[columns_to_drop], axis=1).set_index('geography')
+    # print(student_df.shape)
+    student_df.columns = column_names
+    # normalize the data
+    student_df = student_df.div(student_df.sum(axis=1), axis=0)
+
+    merged_df = pd.merge(norm_age_df[[21]], student_df, left_index=True, right_index=True)
+
+    # take values (remove index) and reshape so it has one column (otherwise, it has (x, ) shape) and doesn't work :(
+    X = merged_df['student_population'].values.reshape(-1, 1)
+
+    # print(X.values.reshape(-1, 1).shape)
+    y = merged_df[21].values
+
+    model = LinearRegression()
+    model.fit(X, y)
+
+    y_pred = model.predict(X)
+
+    plt.figure(figsize=(8, 6))
+    plt.scatter(y, y_pred)
+    plt.xlabel('Actual Percentage of 21-year-olds')
+    plt.ylabel('Predicted Percentage of 21-year-olds')
+    plt.title('Correlation between Actual and Predicted Percentage of 21-year-olds')
+    plt.show()
+
+    r2 = r2_score(y, y_pred)
+    print(f"R-squared: {r2}")
+
+    corr, _ = pearsonr(y, y_pred)
+    print(f"Correlation: {corr}")
+
+    # ALTERNATIVE SOLUTION WITH THE OTHER LIBRARY YOU USE
+    m_linear = sm.OLS(y, X)
+    results = m_linear.fit()
+
+    y_pred_linear = results.get_prediction(X).summary_frame(alpha=0.05)
+
+    plt.figure(figsize=(8, 6))
+    plt.scatter(y, y_pred_linear["mean"])
+    plt.xlabel('Actual Percentage of 21-year-olds')
+    plt.ylabel('Predicted Percentage of 21-year-olds')
+    plt.title('Correlation between Actual and Predicted Percentage of 21-year-olds')
+    plt.show()
+
+    r2_alternative = r2_score(y, y_pred_linear["mean"])
+    print(f"R-squared: {r2_alternative}")
+    print(results.summary())
+
+    return r2, corr
+
+def predict_age_profile(city_nssec, best_model_for_age):
+    X_city = np.array([city_nssec[col] for col in cols]).reshape(1, -1)
+    predicted_profile = np.zeros(100)
+    for age in range(100):
+        model = best_model_for_age[age]
+        predicted_profile[age] = model.predict(X_city)[0]
+    return predicted_profile
+
+
+def plot_comparison(predicted_profile, actual_profile, city_name):
+  plt.figure(figsize=(10, 6))
+  plt.plot(range(100), predicted_profile, label="Predicted")
+  plt.plot(range(100), actual_profile, label="Actual")
+  plt.xlabel("Age")
+  plt.ylabel("Proportion")
+  plt.title(f"Age Profile Comparison for {city_name}")
+  plt.legend()
+  plt.show()
