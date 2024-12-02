3a7986bbaa18b654f407a5b2bd98ee58699888b9
Heidar-An <heidaranwari@gmail.com>
2024-12-02 20:58:33 +0000

    k means

--- fynesse/address.py
+++ fynesse/address.py
@@ -1,6 +1,6 @@
 # This file contains code for suporting addressing questions in the data
 
-"""# Here are some of the imports we might expect 
+"""# Here are some of the imports we might expect
 import sklearn.model_selection  as ms
 import sklearn.linear_model as lm
 import sklearn.svm as svm
@@ -16,3 +16,28 @@ import scipy.stats"""
 
 """Address a particular question that arises from the data"""
 
+import random
+import numpy as np
+
+def k_means(data_np, k=3, iterations=75, tolerance=1e-4):
+    random.seed(10)
+    num_points, num_flags = data_np.shape
+    # randomly initialize k centroids
+    centroids = data_np[random.sample(range(num_points), k)]
+
+    for _ in range(iterations):
+        # assign each point to its nearest centroid (calculate distance from each one)
+        distances = np.linalg.norm(data_np[:, np.newaxis] - centroids, axis=2)
+        cluster_groups = np.argmin(distances, axis=1)
+
+        # compute the new centroid by taking the mean of each cluster
+        new_centroids = np.array([
+            data_np[cluster_groups == cluster].mean(axis=0) if np.any(cluster_groups == cluster) else centroids[cluster]
+            for cluster in range(k)
+        ])
+
+        # stop early if there hasn't been a big change between the centroids
+        if np.linalg.norm(new_centroids - centroids) < tolerance:
+            break
+        centroids = new_centroids
+    return cluster_groups, centroids
